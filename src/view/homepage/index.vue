d8b72dd53f92bfd91ff9006c5dbcfa84aeff16ff
master <648045014@qq.com>
2020-10-28 17:25:47 +0800

    添加图片轮播效果

--- src/view/homepage/index.vue
+++ src/view/homepage/index.vue
@@ -36,7 +36,7 @@
 		</el-menu>
 		<div class="welcome"><svg class="icon hint" aria-hidden="true">
 				<use xlink:href="#iconshengyin"></use>
-			</svg> 
+			</svg>
 			<span style="color: #666666;margin-top: 10px;">欢迎来访~</span>
 			<span style="float: right;">
 				<el-input v-show="search_input" v-model="input" @change="search" ></el-input>
@@ -45,11 +45,19 @@
 			</span>
 			</div>
 			<div class="content">
-				<div class="adv"></div>
+				<div class="adv">
+          <template>
+            <el-carousel >
+              <el-carousel-item  v-for="item in img" :key="item">
+                <el-image :src="item" style="width: 100%;height: 100%;"></el-image>
+              </el-carousel-item>
+            </el-carousel>
+          </template>
+        </div>
 				<contented></contented>
 				<h1>kjklk</h1>
 			</div>
-			
+
 
 	</div>
 </template>
@@ -65,6 +73,11 @@
 				activeIndex: '1',
 				input:'',
 				search_input:false,
+        img:['http://zhangshengnian.xyz/40d2d27aef5bb881dc094bd509c2dbca.jpg',
+        'http://zhangshengnian.xyz/3ac984589ce189cb95a5e47cc2088f34.jpg',
+        'http://zhangshengnian.xyz/d92f88c3617b1d8a0cf09aa9bbca2a27.jpeg',
+        'http://zhangshengnian.xyz/6f7c5b7f51d2eb91668dc71831d61a37.jpeg',
+        'http://zhangshengnian.xyz/9a752a10213a24cf2fd74e0ab387e781.jpeg'],
 			}
 		},
 		methods: {
@@ -72,7 +85,7 @@
 		        console.log(key, keyPath);
 				if(key==9){
 					this.search_input=true;
-					
+
 				}else{
 					this.search_input=false;
 				}
@@ -83,8 +96,11 @@
 				  adminaudit().then(response => {
 				    console.log(response.data)
 				  })
-			  }
-			  
+			  },
+        handleSelect(key, keyPath) {
+                console.log(key, keyPath);
+              }
+
 		    }
 	}
 </script>
@@ -153,4 +169,19 @@
 		width: 860px;
 		background-color: #0086B3;
 	}
+  .el-carousel__item h3 {
+      color: #475669;
+      font-size: 18px;
+      opacity: 0.75;
+      line-height: 300px;
+      margin: 0;
+    }
+
+    .el-carousel__item:nth-child(2n) {
+      background-color: #99a9bf;
+    }
+
+    .el-carousel__item:nth-child(2n+1) {
+      background-color: #d3dce6;
+    }
 </style>
